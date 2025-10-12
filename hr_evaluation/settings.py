135cf72bfa4ce495309c1e6fccfe5aab07f05561
Mohamed7422 <41089518+Mohamed7422@users.noreply.github.com>
2025-10-12 16:43:35 +0300

    update setting for CORS

--- hr_evaluation/settings.py
+++ hr_evaluation/settings.py
@@ -109,6 +109,26 @@ CORS_ALLOW_ALL_ORIGINS = True
 # If your frontend sends cookies / Authorization header:
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
